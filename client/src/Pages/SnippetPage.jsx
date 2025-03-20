9d14802e525cca24936b6ed4e71921ae5c0ab5de
codeByHimanshu <115619706+codeByHimanshu@users.noreply.github.com>
2025-03-20 17:41:22 +0530

    added user

--- client/src/Pages/SnippetPage.jsx
+++ client/src/Pages/SnippetPage.jsx
@@ -1,36 +1,34 @@
+import { useState } from "react";
 import LoggedUser from "../components/LoggedUser";
 
 export default function SnippetPage() {
-    const [user,setUser]=([{     socketId:1001, username:"himanshu singh"},
-        
-            { socketId:2002, username:"Aman khare"}  ]
-    )
+  const [user, setUser] = useState([
+    { socketId: 1001, username: "Himanshu Singh" },
+    { socketId: 2002, username: "Aman Khare" },
+  ]);
 
-
-    return (
-      <div className="flex h-screen items-center bg-gray-900 p-4">
+  return (
+    <div className="flex h-screen items-center bg-gray-900 p-4">
     
-        <div className="w-1/10 flex items-start animate__animated animate__bounceIn min-h-full border-r-2 border-solid border-gray-50">
-          <img
-            src="/src/assets/snippet_bash_log.png"
-            alt="Snippet Bash Logo"
-            className="w-full max-w-[350px]"
-          />
+      <div className="w-1/10 flex flex-col items-start animate__animated animate__bounceIn min-h-full border-r-2 border-solid border-gray-50">
+        <img
+          src="/src/assets/snippet_bash_log.png"
+          alt="Snippet Bash Logo"
+          className="w-full max-w-[350px]"
+        />
 
-          <div>    
-            {user.map(()=>{
-                <LoggedUser
-                key = {user.socketId}
-                username={user.username}
-                />
-            })}
-          </div>
-        </div>
-        
-      
-        <div className="w-9/10 p-6 bg-gray-900 shadow-lg rounded-lg animate__animated animate__fadeInRight h-full">
-          <h1 className="text-2xl font-bold text-gray-800">Write your code here</h1>
+  
+        <div>
+          {user.map(({ socketId, username }) => (
+            <LoggedUser key={socketId} username={username} />
+          ))}
         </div>
       </div>
-    );
-  } 
+
+  
+      <div className="w-9/10 p-6 bg-gray-900 shadow-lg rounded-lg animate__animated animate__fadeInRight h-full">
+        <h1 className="text-2xl font-bold text-gray-800">Write your code here</h1>
+      </div>
+    </div>
+  );
+}
--- client/src/components/LoggedUser.jsx
+++ client/src/components/LoggedUser.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { FaUserCircle } from "react-icons/fa";
 
 const LoggedUser = ({username}) => {
   return (
     <div>
       <div>
+
+
+<FaUserCircle size={48} color="gray" />
+
         {username}
       </div>
     </div>
