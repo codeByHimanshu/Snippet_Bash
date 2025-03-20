6a5702a450a1f844e4f24cc9e0737b3a229935f8
codeByHimanshu <115619706+codeByHimanshu@users.noreply.github.com>
2025-03-20 17:30:04 +0530

    created loogeduser component

--- client/src/Pages/SnippetPage.jsx
+++ client/src/Pages/SnippetPage.jsx
@@ -1,13 +1,30 @@
+import LoggedUser from "../components/LoggedUser";
+
 export default function SnippetPage() {
+    const [user,setUser]=([{     socketId:1001, username:"himanshu singh"},
+        
+            { socketId:2002, username:"Aman khare"}  ]
+    )
+
+
     return (
       <div className="flex h-screen items-center bg-gray-900 p-4">
     
-        <div className="w-1/10 flex justify-center items-center animate__animated animate__bounceIn min-h-full border-r-2 border-solid border-gray-50">
+        <div className="w-1/10 flex items-start animate__animated animate__bounceIn min-h-full border-r-2 border-solid border-gray-50">
           <img
             src="/src/assets/snippet_bash_log.png"
             alt="Snippet Bash Logo"
-            className="w-full max-w-[150px]"
+            className="w-full max-w-[350px]"
           />
+
+          <div>    
+            {user.map(()=>{
+                <LoggedUser
+                key = {user.socketId}
+                username={user.username}
+                />
+            })}
+          </div>
         </div>
         
       
@@ -16,4 +33,4 @@ export default function SnippetPage() {
         </div>
       </div>
     );
-  }
+  } 
---
+++ client/src/components/LoggedUser.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoggedUser = ({username}) => {
+  return (
+    <div>
+      <div>
+        {username}
+      </div>
+    </div>
+  )
+}
+
+export default LoggedUser
