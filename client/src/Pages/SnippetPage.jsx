051d72c17ea9b669c89daf7fd1f120ae4e2d0f03
codeByHimanshu <115619706+codeByHimanshu@users.noreply.github.com>
2025-03-21 13:10:27 +0530

    code editor

--- client/src/Pages/SnippetPage.jsx
+++ client/src/Pages/SnippetPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import LoggedUser from "../components/LoggedUser";
+import TextEditor from "../components/TextEditor";
 
 export default function SnippetPage() {
   const [user, setUser] = useState([
@@ -28,6 +29,7 @@ export default function SnippetPage() {
   
       <div className="w-9/10 p-6 bg-gray-900 shadow-lg rounded-lg animate__animated animate__fadeInRight h-full">
         <h1 className="text-2xl font-bold text-gray-800">Write your code here</h1>
+        <TextEditor />
       </div>
     </div>
   );
---
+++ client/src/components/TextEditor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TextEditor = () => {
+  return (
+    <div>
+      write your code here
+    </div>
+  )
+}
+
+export default TextEditor
